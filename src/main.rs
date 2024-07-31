e85e326424e035d76bd7268e62754f61fbe5ca36
Sunny Chen <z5267282@student.unsw.edu.au>
2024-07-31 15:29:42 +1000

    adding main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+mod q1;
+
+fn run_q1() -> Result<(), String> {
+    let ans = q1::q1::solve().ok_or("Could not read file for q1")?;
+    println!("answer : {ans}");
+    Ok(())
+}
+
+fn main() -> Result<(), String> {
+    run_q1()
 }
