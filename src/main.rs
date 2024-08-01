f6478e45361e98835d09fc8831927905bae94ff6
z5267282 <80458779+z5267282@users.noreply.github.com>
2024-08-01 23:51:23 +1000

    purging and redoing code

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,6 @@
 mod q1;
 
-#[allow(unused)]
-fn run_q1() -> Result<(), String> {
-    let ans = q1::q1::solve().ok_or("Could not read file for q1")?;
-    println!("answer : {ans}");
-    Ok(())
-}
-
-fn main() -> Result<(), String> {
-    q1::q1::part_2();
-    Ok(())
+fn main() {
+    // q1::q1::part_1::run();
+    q1::q1::part_2::run();
 }
--- src/q1/q1.rs
+++ src/q1/q1.rs
@@ -1,105 +1,119 @@
-use std::cmp::{max, min};
-use std::collections::HashMap;
 use std::fs::read_to_string;
 
-// const FILE_PATH: &str = "src/q1/input.txt";
-const FILE_PATH: &str = "src/q1/small.txt";
+const FILE_PATH: &str = "src/q1/one.txt";
 
-pub fn solve() -> Option<u32> {
-    let contents = read_to_string(FILE_PATH).ok()?;
-    let mut result: u32 = 0;
-    for line in contents.lines() {
-        let numbers = line
-            .chars()
-            .filter_map(|c| c.to_digit(10))
-            .collect::<Vec<u32>>();
-        let first = numbers.get(0).unwrap_or(&0);
-        let last = numbers.get(numbers.len() - 1).unwrap_or(&0);
-        result += first * 10 + last;
-    }
-    Some(result)
+fn open_file() -> Option<String> {
+    read_to_string(FILE_PATH).ok()
 }
 
-pub fn part_2() {
-    match read_to_string(FILE_PATH).ok() {
-        Some(contents) => {
-            let res = _part_2(contents);
-            println!("answer : {res}");
+#[allow(unused)]
+pub mod part_1 {
+    use super::open_file;
+
+    pub fn run() {
+        if let Some(contents) = open_file() {
+            println!("{}", solve(contents))
         }
-        None => println!("could not open {FILE_PATH}"),
+    }
+
+    fn solve(contents: String) -> u32 {
+        let mut result: u32 = 0;
+        for line in contents.lines() {
+            let numbers = line
+                .chars()
+                .filter_map(|c| c.to_digit(10))
+                .collect::<Vec<u32>>();
+            let first = numbers.get(0).unwrap_or(&0);
+            let last = numbers.get(numbers.len() - 1).unwrap_or(&0);
+            result += first * 10 + last;
+        }
+        result
     }
 }
 
-pub fn _part_2(contents: String) -> i32 {
-    let mut j = 1;
-    let mut words = HashMap::<i32, String>::new();
-    [
-        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-    ]
-    .iter()
-    .for_each(|word| {
-        words.insert(j, word.to_string());
-        j += 1;
-    });
+#[allow(unused)]
+pub mod part_2 {
+    use std::cmp::{max, min};
+    use std::collections::HashMap;
 
-    for (word, number) in words.iter() {
-        println!("{word}={number}");
+    use super::open_file;
+
+    pub fn run() {
+        match open_file() {
+            Some(contents) => println!("{}", solve(contents)),
+            None => println!("could not open file"),
+        }
     }
 
-    let mut result = 0;
-    // first occuring index for the value
-    let mut left = HashMap::<i32, usize>::new();
-    // last occuring
-    let mut right = HashMap::<i32, usize>::new();
-    for line in contents.lines() {
-        println!("line : {line}");
-        (0..=100).for_each(|i| {
-            if let Some(word) = words.get(&i) {
-                if let Some(idx) = line.find(word) {
+    fn solve(contents: String) -> i32 {
+        let words = gen_word_mapping();
+        let mut result = 0;
+        // first occuring index for the value
+        for line in contents.lines() {
+            (0..=100).for_each(|i| {
+                let mut left = HashMap::<i32, usize>::new();
+                // last occuring
+                let mut right = HashMap::<i32, usize>::new();
+                if let Some(word) = words.get(&i) {
+                    if let Some(idx) = line.find(word) {
+                        left.entry(i)
+                            .and_modify(|pos| *pos = min(*pos, idx))
+                            .or_insert(idx);
+                    }
+                }
+                if let Some(word) = words.get(&i) {
+                    if let Some(idx) = line.rfind(word) {
+                        right
+                            .entry(i)
+                            .and_modify(|pos| *pos = max(*pos, idx))
+                            .or_insert(idx);
+                    }
+                }
+                let word = i.to_string();
+                if let Some(idx) = line.find(&word) {
                     left.entry(i)
                         .and_modify(|pos| *pos = min(*pos, idx))
                         .or_insert(idx);
                 }
-            }
-            if let Some(word) = words.get(&i) {
-                if let Some(idx) = line.rfind(word) {
+                if let Some(idx) = line.rfind(&word) {
                     right
                         .entry(i)
                         .and_modify(|pos| *pos = max(*pos, idx))
                         .or_insert(idx);
                 }
-            }
-            let word = i.to_string();
-            if let Some(idx) = line.find(&word) {
-                left.entry(i)
-                    .and_modify(|pos| *pos = min(*pos, idx))
-                    .or_insert(idx);
-            }
-            if let Some(idx) = line.rfind(&word) {
-                right
-                    .entry(i)
-                    .and_modify(|pos| *pos = max(*pos, idx))
-                    .or_insert(idx);
-            }
-            let mut first = left
-                .iter()
-                .map(|(i, idx)| (*i, *idx))
-                .collect::<Vec<(i32, usize)>>();
-            first.sort_by(|(_, b), (_, d)| b.cmp(d));
-            let f = first.first().unwrap_or(&(0, 0));
-            let (fi, _) = f;
-            result += *fi;
+                let mut first = left
+                    .iter()
+                    .map(|(i, idx)| (*i, *idx))
+                    .collect::<Vec<(i32, usize)>>();
+                first.sort_by(|(_, b), (_, d)| b.cmp(d));
+                let f = first.first().unwrap_or(&(0, 0));
+                let (fi, _) = f;
+                result += *fi;
 
-            let mut last = right
-                .iter()
-                .map(|(i, idx)| (*i, *idx))
-                .collect::<Vec<(i32, usize)>>();
-            last.sort_by(|(a, _), (c, _)| c.cmp(a));
-            let l = first.first().unwrap_or(&(0, 0));
-            let (li, _) = l;
-            result += *li;
-            println!("  {fi},{li}");
-        })
+                let mut last = right
+                    .iter()
+                    .map(|(i, idx)| (*i, *idx))
+                    .collect::<Vec<(i32, usize)>>();
+                last.sort_by(|(a, _), (c, _)| c.cmp(a));
+                let l = first.first().unwrap_or(&(0, 0));
+                let (li, _) = l;
+                result += *li;
+            })
+        }
+        result
     }
-    result
+
+    fn gen_word_mapping() -> HashMap<i32, String> {
+        let mut j = 1;
+        let mut words = HashMap::<i32, String>::new();
+        [
+            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        ]
+        .iter()
+        .for_each(|word| {
+            words.insert(j, word.to_string());
+            j += 1;
+        });
+        words
+    }
 }
