1d0040c9f4582bd35b3ffd0dc9014a1e8245cc52
z5267282 <80458779+z5267282@users.noreply.github.com>
2024-08-13 17:30:28 +1000

    q3 part 1 working

--- src/q3/q3.py
+++ src/q3/q3.py
@@ -1,28 +1,61 @@
 import re
+from dataclasses import dataclass
 
 FILE = "input.txt"
 
-with open(FILE, "r") as f:
-    map = f.read().splitlines()
 
-total = 0
-for row, line in enumerate(map):
-    for number in re.finditer(r"[0-9]+", line):
-        start, end = number.span()
+@dataclass
+class Number:
+    row: int
+    start: int  # inclusive
+    end: int  # exlusice
+    value: int
 
 
-def is_adjacent(board: list[str], row, start: int, end: int) -> bool:
-    if row > 0:
-        above = board[row - 1][max(0, start - 1):min(len(line), end)]
-        if has_symbols(above):
+def main():
+    with open(FILE, "r") as f:
+        map = f.read().splitlines()
+
+    numbers: list[Number] = []
+    # hashes of coordinates
+    symbols: set[str] = set()
+    # preprocessing
+    for row, line in enumerate(map):
+        # numbers
+        for number in re.finditer(r"[0-9]+", line):
+            start, end = number.span()
+            value = int(number.group(0))
+            numbers.append(Number(row, start, end, value))
+
+        # symbols
+        for j, char in enumerate(line):
+            if not char.isdigit() and char != '.':
+                symbols.add(myhash(row, j))
+
+    # compute answer
+    result = 0
+    for number in numbers:
+        if is_adjacent(symbols, number):
+            result += number.value
+
+    print(f"answer : {result}")
+
+
+def myhash(x: int, y: int) -> str:
+    return f"{x},{y}"
+
+
+def is_adjacent(symbols: set[str], number: Number) -> bool:
+    for j in range(number.start - 1, number.end + 1):
+        above = myhash(number.row - 1, j)
+        below = myhash(number.row + 1, j)
+        if above in symbols or below in symbols:
             return True
-    if row < len(board) - 1:
-        below = board[row + 1][max(0, start - 1):min(len(line), end)]
-        if has_symbols(below):
-            return True
-    # left col
-    # right col
+
+    left = myhash(number.row, number.start - 1)
+    right = myhash(number.row, number.end)
+    return left in symbols or right in symbols
 
 
-def has_symbols(string: str) -> bool:
-    return any(not (s.isdigit() and s == '.') for s in string)
+if __name__ == "__main__":
+    main()
